aa68662d67b56664ee93264329b428d716107784
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2011-10-11 18:57:20 -0500

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,15 @@
 class PagesController < ApplicationController
 
   def login
+    @title = "Login"
   end
 
   def profile
+    @title = "Contact"
   end
 
   def sprints
+    @title = "Sprints"
   end
 
 end
