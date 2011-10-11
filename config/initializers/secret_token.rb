4f2f7411acaa816efbf7d446f97cc895a9691913
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2011-10-11 18:09:28 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CmTestApp::Application.config.secret_token = '1a825789ec048d8a01d6aaf9f183fef194ad92b2766adeab16129216e89c9b519c1a8138fa398e7e61791290280d835f09b121c4292ad4207ea651a72fc4ffc5'
